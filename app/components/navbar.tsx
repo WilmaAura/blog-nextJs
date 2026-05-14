ea9bcf62b41c53e297be3f6950aba9742e7d531d
wilma <mornlitoralis@gmail.com>
2026-05-14 19:29:07 +0700

    some changes on navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,12 +1,20 @@
 import ThemeToggle from "./themeToggle";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-14 px-5 py-6 flex justify-between items-center fixed bg-white">
+    <nav className="w-full h-14 px-5 py-6 flex justify-between items-center fixed bg-stone-100">
       <div className="font-bold text-xl tracking-tighter">WilmaAura</div>
       <div className="space-x-6 text-sm text-gray500">
-        <a href="#">Blog</a>
-        <a href="#">Cerpen</a>
+        <Link href="/" className="hover:text-orange-600 transition-colors">
+          Home
+        </Link>
+        <Link href="/blog" className="hover:text-orange-600 transition-colors">
+          Blog
+        </Link>
+        <Link href="/" className="hover:text-orange-600 transition-colors">
+          Cerpen
+        </Link>
       </div>
     </nav>
   );
