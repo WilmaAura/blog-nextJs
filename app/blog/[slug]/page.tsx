74fe3aaa05abf4772078bd34afb2736f69d4033c
wilma <mornlitoralis@gmail.com>
2026-05-15 16:23:43 +0700

    add mdxRemote because the markdown cant have a space

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import { posts } from "@/data/posts";
 import { notFound } from "next/navigation";
+import { getPostData } from "@/lib/utils";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-// Di Next.js terbaru, params itu bersifat Promise, jadi kita pakai async
 export default async function DetailBlog({
   params,
 }: {
@@ -9,10 +10,10 @@ export default async function DetailBlog({
 }) {
   // 1. Tunggu (await) params-nya dulu biar dapet slug yang bener
   const resolvedParams = await params;
-  const slug = resolvedParams.slug;
+  // const slug = resolvedParams.slug;
 
   // 2. Cari di data posts
-  const post = posts.find((p) => p.slug === slug);
+  const post = await getPostData(resolvedParams.slug);
 
   // 3. Kalau nggak ketemu, lempar ke 404
   if (!post) {
@@ -30,10 +31,7 @@ export default async function DetailBlog({
 
       <div className="mt-12 leading-relaxed text-lg text-neutral-800 space-y-6">
         {/* Ini simulasi konten, nanti bisa kita ganti pake Markdown */}
-        <p>
-          Bagian ini nantinya akan berisi teks lengkap dari cerpen atau opini
-          kamu. Sekarang kita tes dulu apakah slug-nya sudah jalan atau belum.
-        </p>
+        <MDXRemote source={post.content} />
       </div>
     </article>
   );
