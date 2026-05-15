cb7ba7fe1ac11b8c6f4050aff474bf547e080043
wilma <mornlitoralis@gmail.com>
2026-05-15 18:58:16 +0700

    add documentation on navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,16 +2,21 @@
 
 import ThemeToggle from "./themeToggle";
 import Link from "next/link";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react"; /* 
+useState: menyimpan state
+useEffect: menjalankan logic setelah component muncul
+useRef: menyimpan value tanpa re-render
+*/
 
 export default function Navbar() {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(true); //menentukan navbar terlihat atau tidak (true: navbar terlihat)
   const lastScrolly = useRef(0); //Menyimpan posisi scroll terakhir tanpa trigger re-render
 
   useEffect(() => {
     let timeout; //It's not even an error, its just typescript needs a type of the variable
+    //function yang dijalankan setiap user scroll
     const handleScroll = () => {
-      const currentScrolly = window.scrollY;
+      const currentScrolly = window.scrollY; //Ambil posisi scroll browser secarang
       // Jika scroll ke baweh dan sudah melewati area atas (misal > 50px), sembunyikan navbar
       if (currentScrolly > lastScrolly.current && currentScrolly > 50) {
         setVisible(false);
@@ -22,11 +27,11 @@ export default function Navbar() {
 
       timeout = setTimeout(() => {
         setVisible(true);
-      }, 50);
+      }, 300); //tunggu 300ms baru muncul navbarnya
     };
     window.addEventListener("scroll", handleScroll);
 
-    //Membersihkan evet listener saaat komponen tidak dipakai
+    //Membersihkan event listener saaat komponen tidak dipakai
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
@@ -34,7 +39,7 @@ export default function Navbar() {
     <nav
       className={`w-full h-14 px-6 flex justify-between items-center fixed top-0 bg-stone-100/80 backdrop-blur-md z-50 border-b border-neutral-200/50 
         transition-transform duration-300 ease-in-out
-        ${visible ? "translate-y-0" : "-translate-y-full"}`}
+        ${visible ? "translate-y-0" : "-translate-y-full"}`} // jika visible === true maka keliatan navbarnya (translate-y-0). Else: jika visible === false maka navbar digeser keatas sejauh tinggi penuh navbar.
     >
       <Link
         href="/"
